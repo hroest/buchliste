b8b2c5bba9121fb9ba299814d386bde43fb22995
hroest_admin <hroest_admin@olymp.(none)>
2010-08-02 20:48:55 +0200

    SQL to read in a new csv file with books

---
+++ bucherliste.sql
@@ -0,0 +1,34 @@
+create table buch
+( id int auto_increment primary key,
+    ort varchar(255),
+    spalte int,
+    zeile int,
+    autor text,
+    titel text,
+    sprache int,
+    eigner varchar(255),
+    buch_art varchar(255)
+);
+
+create table buch_tmp
+(
+    ort varchar(255),
+    spalte int,
+    zeile int,
+    autor text,
+    titel text,
+    sprache int,
+    eigner varchar(255),
+    buch_art varchar(255)
+);
+
+load data local infile 'bucherliste.csv' into table buch_tmp
+fields terminated by ','
+enclosed by '"'
+;
+
+
+
+insert into buch  (ort  , spalte , zeile , autor, titel , sprache , eigner ,
+    buch_art ) select * from buch_tmp where not titel = '';  
+
